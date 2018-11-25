63f6af5cd895f41755ee795e045becc57b287efb
Evan <jpayne0061@gmail.com>
2018-11-24 23:18:21 -0500

    first commit

--- fileLoader/Program.cs
+++ fileLoader/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace fileLoader
 {
@@ -7,6 +10,79 @@ namespace fileLoader
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Init();
+        }
+
+        public static void Init()
+        {
+
+            string connectionString = @"Data Source =.\SQLEXPRESS; Initial Catalog = LouisvilleData; Integrated Security = True;";
+            int counter = 0;
+
+                string query = "SET ANSI_WARNINGS  OFF Insert Into dbo.SalaryData_89 " +
+        "VALUES(@SalaryDataID, @CalendarYear, @EmployeeName, @Department, @JobTitle, @AnnualRate, @RegularRate, @OvertimeRate, @IncentiveAllowance,  @Other, @YearToDate )";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                TextReader reader = new StreamReader(@"C:\Users\Administrator\Documents\data_sets\SalaryData_89.csv");
+                var csvReader = new CsvReader(reader);
+
+                csvReader.Configuration.BadDataFound = context =>
+                {
+                    System.IO.File.AppendAllText(@"C:\Users\Administrator\Source\Repos\fileLoader\fileLoader\errors.txt", context.RawRow.ToString() + Environment.NewLine);
+                };
+
+                var records = csvReader.GetRecords<Record>();
+
+                connection.Open();
+                foreach (Record record in records)
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        if (counter == 0)
+                        {
+                            counter++;
+                            continue;
+                        }
+                        counter++;
+                        // add parameters and their values
+                        cmd.Parameters.AddWithValue("@AnnualRate", record.AnnualRate);
+                        cmd.Parameters.AddWithValue("@CalendarYear", record.CalendarYear);
+                        cmd.Parameters.AddWithValue("@Department", record.Department);
+                        cmd.Parameters.AddWithValue("@EmployeeName", record.EmployeeName);
+                        cmd.Parameters.AddWithValue("@IncentiveAllowance", record.IncentiveAllowance);
+                        cmd.Parameters.AddWithValue("@JobTitle", record.JobTitle);
+                        cmd.Parameters.AddWithValue("@Other", record.Other);
+                        cmd.Parameters.AddWithValue("@OvertimeRate", record.OvertimeRate);
+                        cmd.Parameters.AddWithValue("@RegularRate", record.RegularRate);
+                        cmd.Parameters.AddWithValue("@SalaryDataID", record.SalaryDataID);
+                        cmd.Parameters.AddWithValue("@YearToDate", record.YearToDate);
+                        // open connection, execute command and close connection
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                        catch (Exception e)
+                        {
+                            try
+                            {
+                                System.IO.File.AppendAllText(@"C:\Users\Administrator\Source\Repos\fileLoader\fileLoader\errors.txt", counter.ToString() + ": " + e.Message + Environment.NewLine);
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+
+                        }
+
+                    }
+                }
+                   
+            }
+
+            System.IO.File.AppendAllText(@"/errors.txt", DateTime.Now.ToString() + " *** END PROCESS " + Environment.NewLine);
+
         }
     }
 }
---
+++ fileLoader/Record.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fileLoader
+{
+    public class Record
+    {
+        public int SalaryDataID { get; set; }
+        public int CalendarYear { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+        public float AnnualRate { get; set; }
+        public float RegularRate { get; set; }
+        public float OvertimeRate { get; set; }
+        public float IncentiveAllowance { get; set; }
+        public float Other { get; set; }
+        public float YearToDate { get; set; }
+
+    }
+}
+
